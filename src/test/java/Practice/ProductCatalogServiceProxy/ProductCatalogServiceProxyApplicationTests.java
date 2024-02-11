e588119f03dbf3f518bbc3bfaffbbeb014ef01cc
Geeta Madhuri <geetamadhurimallidi@gmail.com>
2024-02-11 18:11:29 +0530

    Start: has basic dependencies included

---
+++ src/test/java/Practice/ProductCatalogServiceProxy/ProductCatalogServiceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package Practice.ProductCatalogServiceProxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogServiceProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
